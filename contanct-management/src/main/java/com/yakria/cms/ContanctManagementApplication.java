c384103c54c16a9631a5c3b6d80fe00a25ee831a
hawarijawed <hawarijawed143@gmail.com>
2026-01-31 21:14:00 +0545

    First commit

---
+++ contanct-management/src/main/java/com/yakria/cms/ContanctManagementApplication.java
@@ -0,0 +1,14 @@
+package com.yakria.cms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ContanctManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ContanctManagementApplication.class, args);
+        System.out.println("Hello there");
+	}
+
+}
---
+++ contanct-management/src/test/java/com/yakria/cms/ContanctManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yakria.cms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ContanctManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
